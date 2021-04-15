d280ca9e971a0ed93ac6c460656b3912daee50f1
Lisa Goodell <robotsinboxes@gmail.com>
2021-04-15 09:48:59 -0500

    built out most of readme generator, need to complete and test

---
+++ Develop/index.js
@@ -0,0 +1,80 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMD = require('./generateMD');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+    type: 'input',
+    message: 'What is the name of your project: ',
+    name: 'projectTitle'
+    },
+    {
+    type: 'input',
+    message: "Write a brief description of your project: ",
+    name: 'projectDesc'
+    },
+    {
+    type: 'input',
+    message: 'What does the user need to know about installation: ',
+    name: 'installation'
+    }, 
+    {
+    type: 'input',
+    message: 'What does the user need to know about using the repo: ',
+    name: 'usage'
+    },
+    {
+    type: 'list',
+    message: 'List application licenses: ',
+    name: 'license',
+    choices: [
+        "None",
+        'MIT',
+        "ISC",
+        "Apache 2.0", 
+        ] 
+    },
+    {
+    type: 'input',
+    message: 'What does the user need to know about contributing to the repo: ',
+    name: 'contributing'
+    }, 
+    {
+    type: 'input',
+    message: 'What command is needed to run tests: ',
+    name: 'tests'
+    }, 
+    {
+    type: 'input',
+    message: "What is your Github user name: ",
+    name: 'githubUserName'
+    }, 
+    {
+    type: 'input',
+    message: 'What is your email address: ',
+    name: 'email'
+    },        
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    console.log(fileName);
+    console.log(data);
+    fs.writeFile(fileName, generateMD(data), (err) => {
+        err ? console.error(err) : console.log('You created a README!')
+    })
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((res) => {
+        writeToFile('README.md', res)
+    })
+}
+
+// Function call to initialize app
+init();
---
+++ Develop/utils/generateMD.js
@@ -0,0 +1,64 @@
+// TODO: Create a function that returns a license badge based on which `license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+    switch (license) {
+        case 'None':
+            return "";
+            break;
+        case 'MIT':
+            return 'https://img.shields.io/badge/License-MIT-yellow.svg';
+            break;
+        case 'ISC':
+            return 'https://img.shields.io/badge/License-ISC-blue.svg';
+            break;
+        case 'Apache 2.0':
+            return 'https://img.shields.io/badge/License-Apache%202.0-blue.svg';
+            break;
+    }
+  }
+  
+  // TODO: Create a function that returns the license link
+  // If there is no license, return an empty string
+  function renderLicenseLink(license) {}
+  
+  // TODO: Create a function that returns the license section of README
+  // If there is no license, return an empty string
+  function renderLicenseSection(license) {}
+
+var generateMD = (data) => {
+    return `
+    # ${projectTitle}
+
+    ## Project Description
+    ${projectDesc}
+
+    ## Table of Contents
+    [Installation][#installation]
+    [Usage][#usage]
+    [License][#license]
+    [Contributing][#contributing]
+    [Testing][#testing]
+    [Questions][#questions]
+
+    ## Installation
+    ${installation}
+
+    ## Usage
+    ${usage}
+
+    ## License
+    ${license}
+
+    ## Contributing
+    ${contributing}
+
+    ## Testing
+    ${tests}
+
+    ## Questions
+    ${githubUserName}
+    For additional information, please reach out to me at ${email}
+    `
+}
+
+module.exports = generateMD;
